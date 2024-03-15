58a98397bc7b390a50df016476be174c4ffce207
KacperNarwojsz <kacper.narwojsz@gmail.com>
2024-03-15 13:22:24 +0100

    feature - CthulhuMystery and CthulhuSheet classnames changed to dashed

--- src/Components/CthulhuMystery.js
+++ src/Components/CthulhuMystery.js
@@ -8,56 +8,219 @@ import CthulhuMysteryCard3 from '../InteractiveCards/CthulhuMysteryCard3';
 import CthulhuMysteryCard4 from '../InteractiveCards/CthulhuMysteryCard4';
 import CthulhuMysteryRise from '../InteractiveCards/CthulhuMysteryRise';
 import 'reactjs-popup/dist/index.css';
-import './Components.css'
+import './Components.css';
 
 class CthulhuMystery extends Component {
-    constructor({props, chooseMystery, canPop, discardMysteryDeck, mysteryDecrement, mysteryIncrementFull, mysteryIncrementHalf, mysteryDeck, cthulhuAwakening, mysteryDone, mysteryDecrementMonster, mysteryIncrementMonster}) {
-        super(props);
-        this.chooseMystery = chooseMystery
-        this.canPop = canPop
-        this.discardMysteryDeck = discardMysteryDeck
-        this.mysteryDecrement = mysteryDecrement
-        this.mysteryIncrementFull = mysteryIncrementFull
-        this.mysteryIncrementHalf = mysteryIncrementHalf
-        this.mysteryDeck = mysteryDeck
-        this.cthulhuAwakening = cthulhuAwakening
-        this.mysteryDone = mysteryDone
-        this.mysteryDecrementMonster = mysteryDecrementMonster
-        this.mysteryIncrementMonster = mysteryIncrementMonster
-    }
+  constructor({
+    props,
+    chooseMystery,
+    canPop,
+    discardMysteryDeck,
+    mysteryDecrement,
+    mysteryIncrementFull,
+    mysteryIncrementHalf,
+    mysteryDeck,
+    cthulhuAwakening,
+    mysteryDone,
+    mysteryDecrementMonster,
+    mysteryIncrementMonster,
+  }) {
+    super(props);
+    this.chooseMystery = chooseMystery;
+    this.canPop = canPop;
+    this.discardMysteryDeck = discardMysteryDeck;
+    this.mysteryDecrement = mysteryDecrement;
+    this.mysteryIncrementFull = mysteryIncrementFull;
+    this.mysteryIncrementHalf = mysteryIncrementHalf;
+    this.mysteryDeck = mysteryDeck;
+    this.cthulhuAwakening = cthulhuAwakening;
+    this.mysteryDone = mysteryDone;
+    this.mysteryDecrementMonster = mysteryDecrementMonster;
+    this.mysteryIncrementMonster = mysteryIncrementMonster;
+  }
 
-    render(){
-        return (
-            <div className='ancientMystery'>
-                <div className='ancientMysteryDiscard'>
-                    {this.props.popping?
-                    <Popup onOpen={this.chooseMystery} onClose={this.canPop} contentStyle={{background:'transparent', border: 'transparent'}} trigger=
-                    {<button className={this.props.isLoadDone ? 'ancientMysteryReverse' : 'ancientMysteryReverseStamp'} id={`${this.props.ancient}MysteryReverse`}><p className='mysteryCounterText'>Rozwiązano: {this.discardMysteryDeck.length}<span>/3</span></p></button>}modal nested>
-                    {close => (<div className='outerPopup'><div className='ancientMysteryFrontPopup' id={`${this.props.ancient}MysteryFront${this.props.prevMysteryNo}`}><button className='mysteryCloseButton' onClick={() => close()}>X</button></div></div>)}
-                    </Popup>:<button className='ancientMysteryReverse' id={`${this.props.ancient}MysteryReverse`}><p className='mysteryCounterText'>Rozwiązano: {this.discardMysteryDeck.length}<span>/3</span></p></button>}
-                    <div className='ancientMysteryDiscardButtonDiv'>
-                        <Popup contentStyle={{background:'transparent', border: 'transparent'}} trigger=
-                            {this.discardMysteryDeck.length!==0?<button className='discardButton' id='discardMystery'>ODRZUCONE</button>:null}modal nested>
-                            {close => (<div className='outerPopupDiscard'>
-                            <button className='mysteryDiscardCloseButton' onClick={() => close()}>X</button>
-                            <Swiper effect={'cards'} grabCursor={true} modules={[EffectCards]} className="mySwiper" id="swiperMystery">
-                                {this.discardMysteryDeck.length>=1?<SwiperSlide><div className='mysteryCardDiscard' id={`${this.props.ancient}MysteryFront${this.discardMysteryDeck[this.discardMysteryDeck.length-1]}`}></div></SwiperSlide>:null}
-                                {this.discardMysteryDeck.length>=2?<SwiperSlide><div className='mysteryCardDiscard' id={`${this.props.ancient}MysteryFront${this.discardMysteryDeck[this.discardMysteryDeck.length-2]}`}></div></SwiperSlide>:null}
-                                {this.discardMysteryDeck.length>=3?<SwiperSlide><div className='mysteryCardDiscard' id={`${this.props.ancient}MysteryFront${this.discardMysteryDeck[this.discardMysteryDeck.length-3]}`}></div></SwiperSlide>:null}
-                                {this.discardMysteryDeck.length>=4?<SwiperSlide><div className='mysteryCardDiscard' id={`${this.props.ancient}MysteryFront${this.discardMysteryDeck[this.discardMysteryDeck.length-4]}`}></div></SwiperSlide>:null}
-                            </Swiper>
-                            </div>)}
-                        </Popup>
-                    </div>
+  render() {
+    return (
+      <div className="ancient-mystery">
+        <div className="ancient-mystery-discard">
+          {this.props.popping ? (
+            <Popup
+              onOpen={this.chooseMystery}
+              onClose={this.canPop}
+              contentStyle={{ background: 'transparent', border: 'transparent' }}
+              trigger={
+                <button
+                  className={this.props.isLoadDone ? 'ancient-mystery-reverse' : 'ancient-mystery-reverse-stamp'}
+                  id={`${this.props.ancient}MysteryReverse`}
+                >
+                  <p className="mystery-counter-text">
+                    Rozwiązano: {this.discardMysteryDeck.length}
+                    <span>/3</span>
+                  </p>
+                </button>
+              }
+              modal
+              nested
+            >
+              {(close) => (
+                <div className="outer-popup">
+                  <div
+                    className="ancient-mystery-front-popup"
+                    id={`${this.props.ancient}MysteryFront${this.props.prevMysteryNo}`}
+                  >
+                    <button className="mystery-close-button" onClick={() => close()}>
+                      X
+                    </button>
+                  </div>
                 </div>
-                {this.props.isMystery1Active?<CthulhuMysteryCard1 characters={this.props.characters} mysteryDecrement={this.mysteryDecrement} mysteryCounter={this.props.mysteryCounter} mysteryIncrementFull={this.mysteryIncrementFull} key={this.props.ancientCardFlipped} mysteryDeck={this.mysteryDeck} victory={this.props.victory} ancientCardFlipped={this.props.ancientCardFlipped} mysteryDone={this.props.ancientCardFlipped && this.mysteryDeck.length === 0 ? this.cthulhuAwakening : this.mysteryDone}/>:null} 
-                {this.props.isMystery2Active?<CthulhuMysteryCard2 characters={this.props.characters} mysteryDecrement={this.mysteryDecrement} mysteryCounter={this.props.mysteryCounter} mysteryIncrementHalf={this.mysteryIncrementHalf} key={this.props.ancientCardFlipped} mysteryDeck={this.mysteryDeck} victory={this.props.victory} ancientCardFlipped={this.props.ancientCardFlipped} mysteryDone={this.props.ancientCardFlipped && this.mysteryDeck.length === 0 ? this.cthulhuAwakening : this.mysteryDone}/>:null}
-                {this.props.isMystery3Active?<CthulhuMysteryCard3 characters={this.props.characters} mysteryDecrement={this.mysteryDecrement} mysteryCounter={this.props.mysteryCounter} mysteryIncrementFull={this.mysteryIncrementFull} key={this.props.ancientCardFlipped} mysteryDeck={this.mysteryDeck} victory={this.props.victory} ancientCardFlipped={this.props.ancientCardFlipped} mysteryDone={this.props.ancientCardFlipped && this.mysteryDeck.length === 0 ? this.cthulhuAwakening : this.mysteryDone}/>:null}
-                {this.props.isMystery4Active?<CthulhuMysteryCard4 characters={this.props.characters} mysteryDecrementMonster={this.mysteryDecrementMonster} mysteryCounter={this.props.mysteryCounter} mysteryIncrementMonster={this.mysteryIncrementMonster} key={this.props.ancientCardFlipped} monsterSlayed={this.props.monsterSlayed} mysteryDeck={this.mysteryDeck} victory={this.props.victory} ancientCardFlipped={this.props.ancientCardFlipped} mysteryDone={this.props.ancientCardFlipped && this.mysteryDeck.length === 0 ? this.cthulhuAwakening : this.mysteryDone}/>:null}
-                {this.props.isCthulhuAwake?<CthulhuMysteryRise characters={this.props.characters} cthulhuSlayed={this.props.victory}/>:null}
-            </div>
-        )
-    }
+              )}
+            </Popup>
+          ) : (
+            <button className="ancient-mystery-reverse" id={`${this.props.ancient}MysteryReverse`}>
+              <p className="mystery-counter-text">
+                Rozwiązano: {this.discardMysteryDeck.length}
+                <span>/3</span>
+              </p>
+            </button>
+          )}
+          <div className="ancient-mystery-discard-button-div">
+            <Popup
+              contentStyle={{ background: 'transparent', border: 'transparent' }}
+              trigger={
+                this.discardMysteryDeck.length !== 0 ? (
+                  <button className="discard-button" id="discardMystery">
+                    ODRZUCONE
+                  </button>
+                ) : null
+              }
+              modal
+              nested
+            >
+              {(close) => (
+                <div className="outer-popup-discard">
+                  <button className="mystery-discard-close-button" onClick={() => close()}>
+                    X
+                  </button>
+                  <Swiper
+                    effect={'cards'}
+                    grabCursor={true}
+                    modules={[EffectCards]}
+                    className="mySwiper"
+                    id="swiperMystery"
+                  >
+                    {this.discardMysteryDeck.length >= 1 ? (
+                      <SwiperSlide>
+                        <div
+                          className="mystery-card-discard"
+                          id={`${this.props.ancient}MysteryFront${
+                            this.discardMysteryDeck[this.discardMysteryDeck.length - 1]
+                          }`}
+                        ></div>
+                      </SwiperSlide>
+                    ) : null}
+                    {this.discardMysteryDeck.length >= 2 ? (
+                      <SwiperSlide>
+                        <div
+                          className="mystery-card-discard"
+                          id={`${this.props.ancient}MysteryFront${
+                            this.discardMysteryDeck[this.discardMysteryDeck.length - 2]
+                          }`}
+                        ></div>
+                      </SwiperSlide>
+                    ) : null}
+                    {this.discardMysteryDeck.length >= 3 ? (
+                      <SwiperSlide>
+                        <div
+                          className="mystery-card-discard"
+                          id={`${this.props.ancient}MysteryFront${
+                            this.discardMysteryDeck[this.discardMysteryDeck.length - 3]
+                          }`}
+                        ></div>
+                      </SwiperSlide>
+                    ) : null}
+                    {this.discardMysteryDeck.length >= 4 ? (
+                      <SwiperSlide>
+                        <div
+                          className="mystery-card-discard"
+                          id={`${this.props.ancient}MysteryFront${
+                            this.discardMysteryDeck[this.discardMysteryDeck.length - 4]
+                          }`}
+                        ></div>
+                      </SwiperSlide>
+                    ) : null}
+                  </Swiper>
+                </div>
+              )}
+            </Popup>
+          </div>
+        </div>
+        {this.props.isMystery1Active ? (
+          <CthulhuMysteryCard1
+            characters={this.props.characters}
+            mysteryDecrement={this.mysteryDecrement}
+            mysteryCounter={this.props.mysteryCounter}
+            mysteryIncrementFull={this.mysteryIncrementFull}
+            key={this.props.ancientCardFlipped}
+            mysteryDeck={this.mysteryDeck}
+            victory={this.props.victory}
+            ancientCardFlipped={this.props.ancientCardFlipped}
+            mysteryDone={
+              this.props.ancientCardFlipped && this.mysteryDeck.length === 0 ? this.cthulhuAwakening : this.mysteryDone
+            }
+          />
+        ) : null}
+        {this.props.isMystery2Active ? (
+          <CthulhuMysteryCard2
+            characters={this.props.characters}
+            mysteryDecrement={this.mysteryDecrement}
+            mysteryCounter={this.props.mysteryCounter}
+            mysteryIncrementHalf={this.mysteryIncrementHalf}
+            key={this.props.ancientCardFlipped}
+            mysteryDeck={this.mysteryDeck}
+            victory={this.props.victory}
+            ancientCardFlipped={this.props.ancientCardFlipped}
+            mysteryDone={
+              this.props.ancientCardFlipped && this.mysteryDeck.length === 0 ? this.cthulhuAwakening : this.mysteryDone
+            }
+          />
+        ) : null}
+        {this.props.isMystery3Active ? (
+          <CthulhuMysteryCard3
+            characters={this.props.characters}
+            mysteryDecrement={this.mysteryDecrement}
+            mysteryCounter={this.props.mysteryCounter}
+            mysteryIncrementFull={this.mysteryIncrementFull}
+            key={this.props.ancientCardFlipped}
+            mysteryDeck={this.mysteryDeck}
+            victory={this.props.victory}
+            ancientCardFlipped={this.props.ancientCardFlipped}
+            mysteryDone={
+              this.props.ancientCardFlipped && this.mysteryDeck.length === 0 ? this.cthulhuAwakening : this.mysteryDone
+            }
+          />
+        ) : null}
+        {this.props.isMystery4Active ? (
+          <CthulhuMysteryCard4
+            characters={this.props.characters}
+            mysteryDecrementMonster={this.mysteryDecrementMonster}
+            mysteryCounter={this.props.mysteryCounter}
+            mysteryIncrementMonster={this.mysteryIncrementMonster}
+            key={this.props.ancientCardFlipped}
+            monsterSlayed={this.props.monsterSlayed}
+            mysteryDeck={this.mysteryDeck}
+            victory={this.props.victory}
+            ancientCardFlipped={this.props.ancientCardFlipped}
+            mysteryDone={
+              this.props.ancientCardFlipped && this.mysteryDeck.length === 0 ? this.cthulhuAwakening : this.mysteryDone
+            }
+          />
+        ) : null}
+        {this.props.isCthulhuAwake ? (
+          <CthulhuMysteryRise characters={this.props.characters} cthulhuSlayed={this.props.victory} />
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default CthulhuMystery;
--- src/Components/CthulhuSheet.js
+++ src/Components/CthulhuSheet.js
@@ -1,37 +1,70 @@
 import React, { Component } from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
-import './Components.css'
+import './Components.css';
 
 class CthulhuSheet extends Component {
-    constructor({props, flipCardSheet, sheetCardCthulhuCounterDecrement, sheetCardCthulhuCounterIncrement}) {
-        super(props);
-        this.flipCardSheet = flipCardSheet
-        this.sheetCardCthulhuCounterDecrement = sheetCardCthulhuCounterDecrement
-        this.sheetCardCthulhuCounterIncrement = sheetCardCthulhuCounterIncrement
-    }
+  constructor({ props, flipCardSheet, sheetCardCthulhuCounterDecrement, sheetCardCthulhuCounterIncrement }) {
+    super(props);
+    this.flipCardSheet = flipCardSheet;
+    this.sheetCardCthulhuCounterDecrement = sheetCardCthulhuCounterDecrement;
+    this.sheetCardCthulhuCounterIncrement = sheetCardCthulhuCounterIncrement;
+  }
 
-    render(){
-        return (
-            <>
-                <Popup contentStyle={{background:'none', border: 'none'}} trigger=
-                    {<figure className={this.props.isLoadDone ? 'ancientSheet' : 'ancientSheetStamp'} id={this.props.ancientCardFlipped ? `${this.props.ancient}SheetBack` : `${this.props.ancient}SheetFront`} tabIndex="0">
-                        {this.props.ancientCardFlipped?<button className="sheetCardSanityCounter">{this.props.sheetCardCthulhuCounter}</button>:null}
-                    </figure>}modal nested>
-                    {<div className='ancientSheetFocus' id={this.props.ancientCardFlipped ? `${this.props.ancient}SheetBack` : `${this.props.ancient}SheetFront`}>
-                    {!this.props.ancientCardFlipped?<button className='flipButton' id='flipButtonFrontFocus' onClick={this.flipCardSheet}></button>:null}
-                        {this.props.ancientCardFlipped?<button className='flipButton' id='flipButtonBackFocus' onClick={this.flipCardSheet}></button>:null}
-                        {this.props.ancientCardFlipped?<div className='sheetCardAddons' id='sheetCardCthulhuAddonsFocus'>
-                            <button className='sheetCardTokenMinus' id='sheetCardTokenMinusFocus' onClick={this.sheetCardCthulhuCounterDecrement}></button>
-                            <button className='sheetCardSanity' id='sheetCardSanityFocus'></button>
-                            <button className='sheetCardCounter' id='sheetCardCthulhuCounterFocus'>{this.props.sheetCardCthulhuCounter}</button>
-                            <button className='sheetCardTokenPlus' id='sheetCardTokenPlusFocus' onClick={this.sheetCardCthulhuCounterIncrement}></button>
-                        </div>:null}
-                    </div>}
-                </Popup>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <Popup
+          contentStyle={{ background: 'none', border: 'none' }}
+          trigger={
+            <figure
+              className={this.props.isLoadDone ? 'ancient-sheet' : 'ancient-sheet-stamp'}
+              id={this.props.ancientCardFlipped ? `${this.props.ancient}SheetBack` : `${this.props.ancient}SheetFront`}
+              tabIndex="0"
+            >
+              {this.props.ancientCardFlipped ? (
+                <button className="sheet-card-sanity-counter">{this.props.sheetCardCthulhuCounter}</button>
+              ) : null}
+            </figure>
+          }
+          modal
+          nested
+        >
+          {
+            <div
+              className="ancient-sheet-focus"
+              id={this.props.ancientCardFlipped ? `${this.props.ancient}SheetBack` : `${this.props.ancient}SheetFront`}
+            >
+              {!this.props.ancientCardFlipped ? (
+                <button className="flip-button" id="flipButtonFrontFocus" onClick={this.flipCardSheet}></button>
+              ) : null}
+              {this.props.ancientCardFlipped ? (
+                <button className="flip-button" id="flipButtonBackFocus" onClick={this.flipCardSheet}></button>
+              ) : null}
+              {this.props.ancientCardFlipped ? (
+                <div className="sheet-card-addons" id="sheetCardCthulhuAddonsFocus">
+                  <button
+                    className="sheet-card-token-minus"
+                    id="sheetCardTokenMinusFocus"
+                    onClick={this.sheetCardCthulhuCounterDecrement}
+                  ></button>
+                  <button className="sheet-card-sanity" id="sheetCardSanityFocus"></button>
+                  <button className="sheet-card-counter" id="sheetCardCthulhuCounterFocus">
+                    {this.props.sheetCardCthulhuCounter}
+                  </button>
+                  <button
+                    className="sheet-card-token-plus"
+                    id="sheetCardTokenPlusFocus"
+                    onClick={this.sheetCardCthulhuCounterIncrement}
+                  ></button>
+                </div>
+              ) : null}
+            </div>
+          }
+        </Popup>
+      </>
+    );
+  }
 }
 
 export default CthulhuSheet;
