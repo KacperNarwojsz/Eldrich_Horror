4b767b2b356a0c5df5d506313d4ca3ba760c8e4d
KacperNarwojsz <kacper.narwojsz@gmail.com>
2024-03-15 13:52:09 +0100

    feature - ShubNiggurathMystery and ShubNiggurathSheet classnames changed to dashed

--- src/Components/ShubNiggurathSheet.js
+++ src/Components/ShubNiggurathSheet.js
@@ -1,30 +1,49 @@
 import React, { Component } from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
-import './Components.css'
+import './Components.css';
 
 class ShubNiggurathSheet extends Component {
-    constructor({props, flipCardSheet, sheetCardCounterDecrement, sheetCardCounterIncrement, shubNiggurathAwakening}) {
-        super(props);
-        this.flipCardSheet = flipCardSheet
-        this.sheetCardCounterDecrement = sheetCardCounterDecrement
-        this.sheetCardCounterIncrement = sheetCardCounterIncrement
-        this.shubNiggurathAwakening = shubNiggurathAwakening
-    }
+  constructor({ props, flipCardSheet, sheetCardCounterDecrement, sheetCardCounterIncrement, shubNiggurathAwakening }) {
+    super(props);
+    this.flipCardSheet = flipCardSheet;
+    this.sheetCardCounterDecrement = sheetCardCounterDecrement;
+    this.sheetCardCounterIncrement = sheetCardCounterIncrement;
+    this.shubNiggurathAwakening = shubNiggurathAwakening;
+  }
 
-    render(){
-        return (
-            <>
-                <Popup contentStyle={{background:'none', border: 'none'}} trigger=
-                    {<figure className={this.props.isLoadDone ? 'ancientSheet' : 'ancientSheetStamp'} id={this.props.ancientCardFlipped ? `${this.props.ancient}SheetBack` : `${this.props.ancient}SheetFront`} tabIndex="0"></figure>}modal nested>
-                    {<div className='ancientSheetFocus' id={this.props.ancientCardFlipped ? `${this.props.ancient}SheetBack` : `${this.props.ancient}SheetFront`}>
-                    <button className='flipButton' id='flipButtonFrontFocus' onClick={this.flipCardSheet}></button>
-                        {this.props.ancientCardFlipped?<button className='sheetCardAwakeningButton' onClick={this.shubNiggurathAwakening}>PRZEBUDZENIE</button>:null}
-                    </div>}
-                </Popup>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <Popup
+          contentStyle={{ background: 'none', border: 'none' }}
+          trigger={
+            <figure
+              className={this.props.isLoadDone ? 'ancient-sheet' : 'ancient-sheet-stamp'}
+              id={this.props.ancientCardFlipped ? `${this.props.ancient}SheetBack` : `${this.props.ancient}SheetFront`}
+              tabIndex="0"
+            ></figure>
+          }
+          modal
+          nested
+        >
+          {
+            <div
+              className="ancient-sheet-focus"
+              id={this.props.ancientCardFlipped ? `${this.props.ancient}SheetBack` : `${this.props.ancient}SheetFront`}
+            >
+              <button className="flip-button" id="flipButtonFrontFocus" onClick={this.flipCardSheet}></button>
+              {this.props.ancientCardFlipped ? (
+                <button className="sheet-card-awakening-button" onClick={this.shubNiggurathAwakening}>
+                  PRZEBUDZENIE
+                </button>
+              ) : null}
+            </div>
+          }
+        </Popup>
+      </>
+    );
+  }
 }
 
 export default ShubNiggurathSheet;
