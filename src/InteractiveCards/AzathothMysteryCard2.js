f1cd6c534937c59b11d81cb26a557e49eec7a424
KacperNarwojsz <kacper.narwojsz@gmail.com>
2024-03-15 14:26:17 +0100

    feature - AzathothMysteryCards classnames changed to dashed

--- src/InteractiveCards/AzathothMysteryCard2.js
+++ src/InteractiveCards/AzathothMysteryCard2.js
@@ -1,32 +1,64 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
-import "./InteractiveCards.css"
+import './InteractiveCards.css';
 
-const AzathothMysteryCard2 = ({ characters, mysteryDecrement, mysteryCounter, mysteryIncrementHalf, mysteryDeck, victory, ancientCardFlipped, mysteryDone }) => {
-
-    return (
-        <div className="ancientMysteryFrontDiv">
-            <Popup contentStyle={{background:'none', border: 'none'}} trigger=
-                {<figure className="ancientMysteryFront" id="AzathothMysteryFront2"></figure>}modal nested>
-                {<div className="ancientMysteryFrontFocus" id="AzathothMysteryFront2"></div>}
-            </Popup>
-            <div className="mysteryTokensDiv">
-                <div className="mysteryCounter">
-                    <button className="tokenMysteryMinus" onClick={mysteryDecrement}></button>
-                    <button className="tokenMysteryEldritch"></button>
-                    <button className="tokenMysteryPlus" onClick={mysteryIncrementHalf}></button>
-                </div>
-                {mysteryCounter!==Math.ceil(characters/2)?<button className="tokenMysteryCounter">{`${mysteryCounter}/${Math.ceil(characters/2)}`}</button>:null}
-                {mysteryCounter===Math.ceil(characters/2)
-                ?mysteryDeck.length === 0 && !ancientCardFlipped
-                ?<Popup contentStyle={{background:'transparent', border: 'transparent'}} trigger=
-                    {<button className="tokenMysteryDone"></button>}modal nested>
-                    {close => (<div className='defeatPopup'><h1 className="winningPopupText">Czy została zakończona Faza Mitów ?</h1><div className="defeatChoiceButtons"><button className="defeatChoiceButton" onClick={victory}>TAK</button><button className="defeatChoiceButton" onClick={() => close()}>NIE</button></div></div>)}
-                </Popup>
-                :<button className="tokenMysteryDone" onClick={mysteryDone}></button>
-                :null}
-            </div>
+const AzathothMysteryCard2 = ({
+  characters,
+  mysteryDecrement,
+  mysteryCounter,
+  mysteryIncrementHalf,
+  mysteryDeck,
+  victory,
+  ancientCardFlipped,
+  mysteryDone,
+}) => {
+  return (
+    <div className="ancient-mystery-front-div">
+      <Popup
+        contentStyle={{ background: 'none', border: 'none' }}
+        trigger={<figure className="ancient-mystery-front" id="AzathothMysteryFront2"></figure>}
+        modal
+        nested
+      >
+        {<div className="ancient-mystery-front-focus" id="AzathothMysteryFront2"></div>}
+      </Popup>
+      <div className="mystery-tokens-div">
+        <div className="mystery-counter">
+          <button className="token-mystery-minus" onClick={mysteryDecrement}></button>
+          <button className="token-mystery-eldritch"></button>
+          <button className="token-mystery-plus" onClick={mysteryIncrementHalf}></button>
         </div>
-    )
-}
+        {mysteryCounter !== Math.ceil(characters / 2) ? (
+          <button className="token-mystery-counter">{`${mysteryCounter}/${Math.ceil(characters / 2)}`}</button>
+        ) : null}
+        {mysteryCounter === Math.ceil(characters / 2) ? (
+          mysteryDeck.length === 0 && !ancientCardFlipped ? (
+            <Popup
+              contentStyle={{ background: 'transparent', border: 'transparent' }}
+              trigger={<button className="token-mystery-done"></button>}
+              modal
+              nested
+            >
+              {(close) => (
+                <div className="defeat-popup">
+                  <h1 className="winning-popup-text">Czy została zakończona Faza Mitów ?</h1>
+                  <div className="defeat-choice-buttons">
+                    <button className="defeat-choice-button" onClick={victory}>
+                      TAK
+                    </button>
+                    <button className="defeat-choice-button" onClick={() => close()}>
+                      NIE
+                    </button>
+                  </div>
+                </div>
+              )}
+            </Popup>
+          ) : (
+            <button className="token-mystery-done" onClick={mysteryDone}></button>
+          )
+        ) : null}
+      </div>
+    </div>
+  );
+};
 export default AzathothMysteryCard2;
