a4cf79f619887ea692c09bab5ab0f3f296cf99d8
KacperNarwojsz <kacper.narwojsz@gmail.com>
2024-03-15 14:50:43 +0100

    feature - ShubNiggurathMysteryCards classnames changed to dashed

--- src/InteractiveCards/ShubNiggurathMysteryCard3.js
+++ src/InteractiveCards/ShubNiggurathMysteryCard3.js
@@ -1,32 +1,64 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
-import "./InteractiveCards.css"
+import './InteractiveCards.css';
 
-const ShubNiggurathMysteryCard3 = ({ characters, mysteryDecrement, mysteryCounter, mysteryIncrementDouble, mysteryDeck, victory, ancientCardFlipped, mysteryDone, }) => {
-
-    return (
-        <div className="ancientMysteryFrontDiv">
-            <Popup contentStyle={{background:'none', border: 'none'}} trigger=
-                {<figure className="ancientMysteryFront" id="ShubNiggurathMysteryFront3"></figure>}modal nested>
-                {<div className="ancientMysteryFrontFocus" id="ShubNiggurathMysteryFront3"></div>}
-            </Popup>
-            <div className="mysteryTokensDiv">
-                <div className="mysteryCounter">
-                    <button className="tokenMysteryMinus" onClick={mysteryDecrement}></button>
-                    <button className="tokenMysteryToughness"></button>
-                    <button className="tokenMysteryPlus" onClick={mysteryIncrementDouble}></button>
-                </div>
-                {mysteryCounter!==characters*2?<button className="tokenMysteryCounter">{`${mysteryCounter}/${characters*2}`}</button>:null}
-                {mysteryCounter===characters*2
-                ?mysteryDeck.length === 0 && !ancientCardFlipped
-                ?<Popup contentStyle={{background:'transparent', border: 'transparent'}} trigger=
-                    {<button className="tokenMysteryDone"></button>}modal nested>
-                    {close => (<div className='defeatPopup'><h1 className="winningPopupText">Czy została zakończona Faza Mitów ?</h1><div className="defeatChoiceButtons"><button className="defeatChoiceButton" onClick={victory}>TAK</button><button className="defeatChoiceButton" onClick={() => close()}>NIE</button></div></div>)}
-                </Popup>
-                :<button className="tokenMysteryDone" onClick={mysteryDone}></button>
-                :null}
-            </div>
+const ShubNiggurathMysteryCard3 = ({
+  characters,
+  mysteryDecrement,
+  mysteryCounter,
+  mysteryIncrementDouble,
+  mysteryDeck,
+  victory,
+  ancientCardFlipped,
+  mysteryDone,
+}) => {
+  return (
+    <div className="ancient-mystery-front-div">
+      <Popup
+        contentStyle={{ background: 'none', border: 'none' }}
+        trigger={<figure className="ancient-mystery-front" id="ShubNiggurathMysteryFront3"></figure>}
+        modal
+        nested
+      >
+        {<div className="ancient-mystery-front-focus" id="ShubNiggurathMysteryFront3"></div>}
+      </Popup>
+      <div className="mystery-tokens-div">
+        <div className="mystery-counter">
+          <button className="token-mystery-minus" onClick={mysteryDecrement}></button>
+          <button className="token-mystery-toughness"></button>
+          <button className="token-mystery-plus" onClick={mysteryIncrementDouble}></button>
         </div>
-    )
-}
+        {mysteryCounter !== characters * 2 ? (
+          <button className="token-mystery-counter">{`${mysteryCounter}/${characters * 2}`}</button>
+        ) : null}
+        {mysteryCounter === characters * 2 ? (
+          mysteryDeck.length === 0 && !ancientCardFlipped ? (
+            <Popup
+              contentStyle={{ background: 'transparent', border: 'transparent' }}
+              trigger={<button className="token-mystery-done"></button>}
+              modal
+              nested
+            >
+              {(close) => (
+                <div className="defeat-popup">
+                  <h1 className="winning-popup-text">Czy została zakończona Faza Mitów ?</h1>
+                  <div className="defeat-choice-buttons">
+                    <button className="defeat-choice-button" onClick={victory}>
+                      TAK
+                    </button>
+                    <button className="defeat-choice-button" onClick={() => close()}>
+                      NIE
+                    </button>
+                  </div>
+                </div>
+              )}
+            </Popup>
+          ) : (
+            <button className="token-mystery-done" onClick={mysteryDone}></button>
+          )
+        ) : null}
+      </div>
+    </div>
+  );
+};
 export default ShubNiggurathMysteryCard3;
--- src/InteractiveCards/ShubNiggurathMysteryRise.js
+++ src/InteractiveCards/ShubNiggurathMysteryRise.js
@@ -1,54 +1,61 @@
 import React, { Component } from 'react';
-import "./InteractiveCards.css"
+import './InteractiveCards.css';
 
 class ShubNiggurathMysteryRise extends Component {
-    constructor({ shubNiggurathSlayed, characters, discardMysteryDeck }) {
-        super(); 
-        this.state = {
-            shubNiggurathSlayed: shubNiggurathSlayed,
-            monsterSlayed: false,
-            characters: characters,
-            discardMysteryDeck: discardMysteryDeck,
-            counter: 0,
-        } 
-    }
+  constructor({ shubNiggurathSlayed, characters, discardMysteryDeck }) {
+    super();
+    this.state = {
+      shubNiggurathSlayed: shubNiggurathSlayed,
+      monsterSlayed: false,
+      characters: characters,
+      discardMysteryDeck: discardMysteryDeck,
+      counter: 0,
+    };
+  }
 
-    counterIncrement = () => {
-        if (this.state.counter < this.state.characters+2 && this.state.discardMysteryDeck.length >= 3) {
-            this.setState ({counter: this.state.counter +1})
-        } else if (this.state.counter === this.state.characters+2 && this.state.discardMysteryDeck.length >= 3){
-            this.setState ({counter: this.state.counter +1})
-            this.setState ({monsterSlayed: true})
-        }
+  counterIncrement = () => {
+    if (this.state.counter < this.state.characters + 2 && this.state.discardMysteryDeck.length >= 3) {
+      this.setState({ counter: this.state.counter + 1 });
+    } else if (this.state.counter === this.state.characters + 2 && this.state.discardMysteryDeck.length >= 3) {
+      this.setState({ counter: this.state.counter + 1 });
+      this.setState({ monsterSlayed: true });
     }
+  };
 
-    counterDecrement = () => {
-        if (this.state.counter !== 0 && this.state.discardMysteryDeck.length >= 3) {
-            this.setState ({counter: this.state.counter -1})
-            this.setState ({monsterSlayed: false})
-        }
+  counterDecrement = () => {
+    if (this.state.counter !== 0 && this.state.discardMysteryDeck.length >= 3) {
+      this.setState({ counter: this.state.counter - 1 });
+      this.setState({ monsterSlayed: false });
     }
+  };
 
-    render() {
-        return (
-                <div className="ancientMysteryFrontDiv">
-                    <div>
-                        <div className="mysteryTokensMonsterDiv">
-                            <button className="mysteryMonster" id={this.state.monsterSlayed?"TokenShubNiggurathSlayed":"TokenShubNiggurath"}></button>
-                            <div className="mysteryTokensMonster">
-                                {this.state.counter===this.state.characters+3?null:<button className="tokenMysteryKilling"></button>}
-                                {this.state.counter===this.state.characters+3?<button className="tokenMysteryMonsterDone" onClick={this.state.shubNiggurathSlayed}></button>:null}
-                            </div>
-                        </div>
-                        <div className='mysteryMonsterAddons'>
-                        <button className='mysteryMonsterTokenMinus' onClick={this.counterDecrement}></button>
-                        <button className='mysteryMonsterTokenHealth'></button>
-                        <button className='mysteryMonsterCounter'>{`${this.state.counter}/${this.state.characters+3}`}</button>
-                        <button className='mysteryMonsterTokenPlus' onClick={this.counterIncrement}></button>
-                        </div>
-                    </div>
-                </div>
-        )
-    }
+  render() {
+    return (
+      <div className="ancient-mystery-front-div">
+        <div>
+          <div className="mystery-tokens-monster-div">
+            <button
+              className="mystery-monster"
+              id={this.state.monsterSlayed ? 'TokenShubNiggurathSlayed' : 'TokenShubNiggurath'}
+            ></button>
+            <div className="mystery-tokens-monster">
+              {this.state.counter === this.state.characters + 3 ? null : (
+                <button className="token-mystery-killing"></button>
+              )}
+              {this.state.counter === this.state.characters + 3 ? (
+                <button className="token-mystery-konster-done" onClick={this.state.shubNiggurathSlayed}></button>
+              ) : null}
+            </div>
+          </div>
+          <div className="mystery-monster-addons">
+            <button className="mystery-monster-tokenMinus" onClick={this.counterDecrement}></button>
+            <button className="mystery-monster-tokenHealth"></button>
+            <button className="mystery-monster-counter">{`${this.state.counter}/${this.state.characters + 3}`}</button>
+            <button className="mystery-monster-tokenPlus" onClick={this.counterIncrement}></button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default ShubNiggurathMysteryRise;
